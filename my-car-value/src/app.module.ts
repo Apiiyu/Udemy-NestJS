137bcd52de03061675e80932265086efb85f75eb
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-03-29 22:01:49 +0700

    Features: Setting up Database Connection

--- my-car-value/src/app.module.ts
+++ my-car-value/src/app.module.ts
@@ -1,9 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
+import { ReportsModule } from './reports/reports.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite', // ? Type of database
+      database: 'database.sqlite', // ? Name of the database file
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // ? Path to the entities
+      synchronize: true, // ? Synchronize the database with the entities
+    }),
+    UsersModule,
+    ReportsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
