04f9c299c5520d6d31ec0c66c58d2326bae685e9
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-03-30 00:03:51 +0700

    Features: Creating Validations for incoming request using pipes

--- my-car-value/src/main.ts
+++ my-car-value/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
+import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // ? Remove all properties that are not defined in the DTO. Or if we explain it as technical, It will remove all properties (extraneous properties) that are not defined in the DTO.
+    }),
+  );
+  await app.listen(1337);
 }
 bootstrap();
