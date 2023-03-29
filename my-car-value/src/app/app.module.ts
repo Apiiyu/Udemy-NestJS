ffdd43a615fa5ddc8432f4c2b4cfd376221c9d4a
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-03-29 22:38:09 +0700

    Features: Creating an Entity and Repository

--- my-car-value/src/app/app.module.ts
+++ my-car-value/src/app/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { AppController } from './controllers/app.controller';
+import { AppService } from './services/app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { UsersModule } from './users/users.module';
-import { ReportsModule } from './reports/reports.module';
+import { UsersModule } from '../users/users.module';
+import { ReportsModule } from '../reports/reports.module';
 
 @Module({
   imports: [
--- my-car-value/src/users/users.module.ts
+++ my-car-value/src/users/users.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
-import { UsersController } from './users.controller';
-import { UsersService } from './users.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersController } from './controllers/users.controller';
+import { UserEntity } from './entities/user.entity';
+import { UsersService } from './services/users.service';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([UserEntity])],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [UsersService],
 })
 export class UsersModule {}
