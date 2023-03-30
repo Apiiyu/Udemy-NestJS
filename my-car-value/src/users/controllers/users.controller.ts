203c28a8e0857db6f6d8c5d2042b0de03aa8c22b
Apiiyu <khoirulloh.rafi2@gmail.com>
2023-03-30 10:59:19 +0700

    Features: Creating and Saving a User

--- my-car-value/src/users/controllers/users.controller.ts
+++ my-car-value/src/users/controllers/users.controller.ts
@@ -1,10 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
+import { UsersService } from '../services/users.service';
 
 @Controller('api/v1/authentication')
 export class UsersController {
+  constructor(private readonly UsersService: UsersService) {}
+
   @Post('/create-user')
   createUser(@Body() body: CreateUserDto) {
-    console.log(body);
+    return this.UsersService.create(body.email, body.password);
   }
 }
--- my-car-value/src/users/services/users.service.ts
+++ my-car-value/src/users/services/users.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../entities/user.entity';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private readonly UserRepository: Repository<UserEntity>,
+  ) {}
+
+  create(email: string, password: string) {
+    const user = this.UserRepository.create({ email, password }); // ? Create function is used to create a new instance of the entity
+
+    return this.UserRepository.save(user); // ? Save function is used to save the entity to the database
+  }
+}
