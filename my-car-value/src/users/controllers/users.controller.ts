21de89783d13411e99db6f081fff141f676c40e8
Rafi <rafi.k@bts.id>
2023-06-29 22:05:55 +0700

    Feature: Trying new features, especially CRUD users on our application

--- my-car-value/src/users/controllers/users.controller.ts
+++ my-car-value/src/users/controllers/users.controller.ts
@@ -1,13 +1,43 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { CreateUserDto } from '../dtos/create-user.dto';
-import { UsersService } from '../services/users.service';
+import { UpdateUserDto } from '../dtos/update-user.dto';
+import { UsersService as Service } from '../services/users.service';
 
 @Controller('api/v1/authentication')
 export class UsersController {
-  constructor(private readonly UsersService: UsersService) {}
+  constructor(private readonly UsersService: Service) {}
 
   @Post('/create-user')
   createUser(@Body() body: CreateUserDto) {
     return this.UsersService.create(body.email, body.password);
+  }
+
+  @Get('/:id')
+  findOne(@Param('id') id: string) {
+    return this.UsersService.findOne(parseInt(id));
+  }
+
+  @Get()
+  findAllUsers(@Query('email') email: string) {
+    return this.UsersService.find(email);
+  }
+
+  @Delete('/:id')
+  removeUser(@Param('id') id: string) {
+    return this.UsersService.remove(parseInt(id));
+  }
+
+  @Patch('/:id')
+  updateUser(@Param('id') id: string, @Body() requestBody: UpdateUserDto) {
+    return this.UsersService.update(parseInt(id), requestBody);
   }
 }
